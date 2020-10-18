2bdb9852743c47e7f19e58351438655dfef64d40
Jcampsala <jcampsala@gmail.com>
2020-10-19 01:14:08 +0200

    Bottom card to create purpose demo

--- lib/pages/blocks_page.dart
+++ lib/pages/blocks_page.dart
@@ -5,44 +5,51 @@ import 'package:purpose_blocs/blocs/purposes/purposes_barrel.dart';
 import 'package:purpose_blocs/models/purpose.dart';
 import 'package:purpose_blocs/pages/custom_in_list.dart';
 
-/*class BlocksPage extends StatelessWidget {
+class BlocksPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<PurposesBloc, PurposesState>(builder: (context, state) {
       print(state);
       if (state is PurposesLoadSuccess) {
         List<Purpose> purposes = state.purposes;
-        print('Change in blocBuilder');
-        print(purposes);
         return Scaffold(
           //body: SinglePurpose(purpose: purposes[0]),
           body: GridView.count(
             crossAxisCount: 2,
             children: List.generate(purposes.length, (index) {
-              return GestureDetector(
-                child: Container(
-                  margin: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(purposes[index].name),
-                ),
-                onTap: () {
-                  PurposesBloc purposesBloc =
-                      BlocProvider.of<PurposesBloc>(context);
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CustomInList(
-                          itemsPerRow: 7,
-                          blockColor: Color.fromARGB(255, 255, 102, 102),
-                          purposesBloc: purposesBloc,
-                          id: purposes[index].id,
+              return Container(
+                margin: EdgeInsets.all(10),
+                child: OpenContainer(
+                    closedBuilder: (_, openContainer) {
+                      return GestureDetector(
+                        child: Center(
+                          child: Text(purposes[index].name),
                         ),
-                      ));
-                },
+                        onTap: openContainer,
+                      );
+                    },
+                    closedColor: Color.fromARGB(255, 255, 102, 102),
+                    openColor: Color.fromARGB(255, 255, 102, 102),
+                    closedElevation: 1,
+                    transitionType: ContainerTransitionType.fade,
+                    closedShape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    openBuilder: (_, closeContainer) {
+                      PurposesBloc purposesBloc =
+                          BlocProvider.of<PurposesBloc>(context);
+                      return CustomInList(
+                        itemsPerRow: 7,
+                        blockColor: Color.fromARGB(255, 255, 102, 102),
+                        purposesBloc: purposesBloc,
+                        id: purposes[index].id,
+                      );
+                    }),
               );
             }),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => _showModalBottomSheet(context),
           ),
         );
       } else if (state is PurposesLoadFailure) {
@@ -60,87 +67,62 @@ import 'package:purpose_blocs/pages/custom_in_list.dart';
       }
     });
   }
-}*/
 
-class BlocksPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<PurposesBloc, PurposesState>(builder: (context, state) {
-      print(state);
-      if (state is PurposesLoadSuccess) {
-        List<Purpose> purposes = state.purposes;
-        return Scaffold(
-          //body: SinglePurpose(purpose: purposes[0]),
-          body: GridView.count(
-            crossAxisCount: 2,
-            children: List.generate(purposes.length, (index) {
-              return Container(
-                margin: EdgeInsets.all(10),
-                child: OpenContainer(
-                  closedBuilder: (_, openContainer) {
-                    return GestureDetector(
-                      child: Center(
-                        child: Text(purposes[index].name),
+  void _showModalBottomSheet(context) {
+    showModalBottomSheet(
+        context: context,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+        builder: (BuildContext bc) {
+          return Container(
+            child: new Wrap(
+              children: <Widget>[
+                Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.deepOrangeAccent)),
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    child: GestureDetector(
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 20, left: 10),
+                            child: Icon(Icons.calendar_today_sharp),
+                          ),
+                          Expanded(
+                              child: Container(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Actividad todo o nada',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                    'Actividades que sólo pueden estar completadas o no')
+                              ],
+                            ),
+                          ))
+                        ],
                       ),
-                      onTap: openContainer,
-                    );
-                  },
-                  closedColor: Color.fromARGB(255, 255, 102, 102),
-                  openColor: Color.fromARGB(255, 255, 102, 102),
-                  closedElevation: 1,
-                  transitionType: ContainerTransitionType.fade,
-                  closedShape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)
-                  ),
-                  openBuilder: (_, closeContainer) {
-                    PurposesBloc purposesBloc = BlocProvider.of<PurposesBloc>(context);
-                    return CustomInList(
-                      itemsPerRow: 7,
-                      blockColor: Color.fromARGB(255, 255, 102, 102),
-                      purposesBloc: purposesBloc,
-                      id: purposes[index].id,
-                    );
-                  }),
-              );
-              /*return GestureDetector(
-                child: Container(
-                  margin: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(purposes[index].name),
+                      onTap: () {
+                        Purpose p = new Purpose(
+                            DateTime.now().microsecondsSinceEpoch.toString());
+                        BlocProvider.of<PurposesBloc>(context)
+                            .add(AddPurpose(p));
+                      },
+                    )),
+                new ListTile(
+                  leading: new Icon(Icons.timer),
+                  title: new Text('Actividad cronometrada'),
+                  onTap: () => {},
                 ),
-                onTap: () {
-                  PurposesBloc purposesBloc =
-                      BlocProvider.of<PurposesBloc>(context);
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CustomInList(
-                          itemsPerRow: 7,
-                          blockColor: Color.fromARGB(255, 255, 102, 102),
-                          purposesBloc: purposesBloc,
-                          id: purposes[index].id,
-                        ),
-                      ));
-                },
-              );*/
-            }),
-          ),
-        );
-      } else if (state is PurposesLoadFailure) {
-        return Scaffold(
-          body: Center(
-            child: Text('Error al cargar propósitos'),
-          ),
-        );
-      } else {
-        return Scaffold(
-          body: Center(
-            child: CircularProgressIndicator(),
-          ),
-        );
-      }
-    });
+              ],
+            ),
+          );
+        });
   }
 }
